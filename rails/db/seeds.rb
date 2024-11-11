14c45b6b3b79f0ed936b800d234a7d61581a3c4c
okaki1999 <okakilikecat@gmail.com>
2024-11-11 22:23:56 +0900

    rubocop

--- rails/db/seeds.rb
+++ rails/db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 ActiveRecord::Base.transaction do
-  user1 = User.create!(name: "テスト太郎", email: "test1@example.com", password: "password", confirmed_at: Time.current)
+  User.create!(name: "テスト太郎", email: "test1@example.com", password: "password", confirmed_at: Time.current)
 end
